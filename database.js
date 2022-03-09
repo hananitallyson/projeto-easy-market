7d696f4a34af1a65754a1e977dd666e7e923d65a
mosiah-adam <mosiah.adame@escolar.ifrn.edu.br>
2022-03-09 14:29:20 -0300

    version 1

---
+++ database.js
@@ -0,0 +1,19 @@
+//Funcionários
+
+funcionario1 = {user: "Akemi", matriculation: "070", password: "777", age: 17};
+funcionario2 = {user: "Luiza", matriculation: "090", password: "999", age: 15};
+
+funcionarios = [funcionario1, funcionario2];
+
+//Produtos
+
+produto1 = {name: "Cop Noddles", id: "001", price: 4.50};
+produto2 = {name: "Pão francês", id: "002", price: 1.00};
+produto3 = {name: "Biscoito de chocolate", id: "003", price: 3.50};
+produto4 = {name: "Confeito", id: "004", price: 0.70};
+
+produtos = [produto1, produto2, produto3, produto4];
+
+//Vetor usado para verificar se o usuário já adicionou o produto pelo menos uma vez (4 indícies instãnciados como false para os respectivos produtos, a começar do 001)
+
+/* checkProdutcs = [false, false, false, false]; */
