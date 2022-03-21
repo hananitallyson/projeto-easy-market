0fd5ef2cc324689af0db53fc347c8ec1ac179388
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2022-03-21 18:10:22 -0300

    Version 5

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,19 @@
 //Variables
 
+var arrayPrice = []
+var checkedRemove = []
 var finalPrice = 0
 var incrementId = 0
 var inputCheckbox
 var lineOptions
-var arrayPrice = []
-var checkedRemove = []
+
 
 
 //Functions
+
+
+    
+    //Function that checks whether the login is valid or not
 
 function checkData() {
     var valueInputMatriculation = document.getElementById('matriculation').value
@@ -16,34 +21,13 @@ function checkData() {
     for (let index = 0; index < funcionarios.length; index++) {
         let aux = funcionarios[index];
         if (aux['matriculation'] == valueInputMatriculation && aux['password'] == valueInputPassword) {
-            alert('Você conseguiu entrar no sistema com sucesso!\nAgora você vai ser redirecionado para outra página')
-            return window.location.href = 'menu.html'
+            return window.location.href = 'home.html'
         }
     }
     alert('Usuário ou senha errados')
 }
 
-function addProduct() {
-    var tableBody = document.getElementById('table-body')
-    var inputId = document.getElementById('input-id').value
-    for (let index = 0; index < produtos.length; index++) {
-        let aux = produtos[index]
-        if (aux['id'] == inputId) {
-            tableBody.innerHTML = tableBody.innerHTML + '<tr class="newLine"><td><input type="checkbox" id="nome' + incrementId + '" name="product" class="boxesSelect" value="select">' + '<label for="nome' + incrementId+ '">' + aux['name'] + '</label>' +'</td><td>' + aux['id'] + '</td><td>' + aux['price'] + '</td></tr>'
-            inputCheckbox = document.querySelectorAll('input[type="checkbox"]')
-            lineOptions = document.querySelectorAll('.newLine')
-            finalPrice = finalPrice + aux['price']
-            arrayPrice.push(aux['price'])
-            checkedRemove.push(false)
-        }
-        buy()
-    }
-    incrementId++;
-}
-
-function buy() {
-    document.getElementById('show-final-price').innerHTML = 'Preço final: ' + finalPrice;
-}
+    //Function that deletes a product
 
 function deleteProduct() {
     for (let index = 0; index < lineOptions.length; index++) {
@@ -52,8 +36,41 @@ function deleteProduct() {
                 $(lineOptions[index]).css("display", "none")
                 finalPrice = finalPrice - arrayPrice[index]
                 checkedRemove[index] = true
+                incrementId--
+                console.log('Increment id no remover: ' + incrementId);
             }
         }
     }
-    buy()
+}
+
+    //Function that adds a line with the given product and its information
+
+function addProduct() {
+    var tableBody = document.getElementById('table-body')
+    var inputId = document.getElementById('input-id').value
+        for (let index = 0; index < produtos.length; index++) {
+            let aux = produtos[index]
+            if (aux['id'] == inputId) {
+                finalPrice = finalPrice + aux['price']
+                tableBody.innerHTML = tableBody.innerHTML + '<tr class="newLine"><td><input type="checkbox" id="nome' + incrementId + '" name="product" class="boxesSelect" value="select">' + '<label for="nome' + incrementId+ '">' + aux['name'] + '</label>' +'</td><td>' + aux['id'] + '</td><td>' + aux['price'] + '</td></tr>'
+                inputCheckbox = document.querySelectorAll('input[type="checkbox"]')
+                lineOptions = document.querySelectorAll('.newLine')
+                arrayPrice.push(aux['price'])
+                checkedRemove.push(false)
+            }
+        }
+
+    incrementId++;
+    console.log('Increment id no adicionar: ' + incrementId);
+}
+
+function makePurchase() {
+    if (incrementId != 0) {
+        alert('Sua compra foi realizada com sucesso!\nO preço total é: ' + finalPrice)
+        alert('Esperamos que você tenha um bom dia e volte sempre :)')
+        return location.reload()
+    }
+    else {
+        alert('Insira algum produto para poder efetuar a compra!')
+    }
 }
