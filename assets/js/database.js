7c7c2397451e7867c16b223299159d691d29be1f
Hanani Tallyson <79944326+hananitallyson@users.noreply.github.com>
2022-03-21 21:06:58 -0300

    Páginas de bebidas e comidas

--- assets/js/database.js
+++ assets/js/database.js
@@ -3,8 +3,9 @@
 funcionario1 = {user: "Akemi", matriculation: "070", password: "777", age: 17};
 funcionario2 = {user: "Luiza", matriculation: "090", password: "999", age: 15};
 funcionario3 = {user: "Hanani", matriculation: "010", password: "111", age: 17};
+funcionario4 = {user: "Daniel", matriculation: "020", password: "222", age: 17};
 
-funcionarios = [funcionario1, funcionario2, funcionario3];
+funcionarios = [funcionario1, funcionario2, funcionario3, funcionario4];
 
 //Produtos
 
@@ -12,8 +13,12 @@ produto1 = {name: "Cop Noddles", id: "001", price: 4.50};
 produto2 = {name: "Pão francês", id: "002", price: 1.00};
 produto3 = {name: "Biscoito de chocolate", id: "003", price: 3.50};
 produto4 = {name: "Confeito", id: "004", price: 0.70};
+produto5 = {name: "Lata de refrigerante", id: "005", price: 2.50};
+produto6 = {name: "Energético", id: "006", price: 7.00};
+produto7 = {name: "Caixa de leite", id: "007", price: 2.00};
+produto8 = {name: "Cerveja", id: "008", price: 5.50};
 
-produtos = [produto1, produto2, produto3, produto4];
+produtos = [produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8];
 
 //Vetor usado para verificar se o usuário já adicionou o produto pelo menos uma vez (4 indícies instãnciados como false para os respectivos produtos, a começar do 001)
 
